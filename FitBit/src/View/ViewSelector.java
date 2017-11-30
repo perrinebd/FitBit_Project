50cc01d5753b0e70c8321f6d08ee4cf5ae510258
bradleylual <bradleyla@g.cofc.edu>
2017-11-30 11:05:02 -0500

    Implementing ViewSelector as a window

--- FitBit/src/View/ViewSelector.java
+++ FitBit/src/View/ViewSelector.java
@@ -12,74 +12,23 @@ import java.awt.event.ItemListener;
 
 public class ViewSelector extends JPanel {
     private Graphics2D currPane;
-    private JFrame currFrame = new JFrame();
-	private JPanel checkBoxPanel = new JPanel();
-	private JCheckBox checkBoxWatch = new JCheckBox();
-	private JCheckBox checkBoxStep = new JCheckBox();
-	private JCheckBox checkBoxHRM = new JCheckBox();
-	private JCheckBox checkBoxUser = new JCheckBox();
+    private JFrame currFrame;
+	private JPanel checkBoxPanel;
+	private JCheckBox checkBoxWatch;
+	private JCheckBox checkBoxStep;
+	private JCheckBox checkBoxHRM;
+	private JCheckBox checkBoxUser;
 
-    public ViewSelector(JFrame currFrame) {
-    	
-		this.currFrame = currFrame;
-		checkBoxPanel.setLayout(new GridLayout(2, 2));
-		checkBoxPanel.add(checkBoxWatch);
-		checkBoxPanel.add(checkBoxStep);
-		checkBoxPanel.add(checkBoxHRM);
-		checkBoxPanel.add(checkBoxUser);
-	
-		checkBoxWatch.addItemListener(new ItemListener() {
-		
-			@Override
-			public void itemStateChanged(ItemEvent e) {
-				if (checkBoxWatch.isSelected()) {
-					StopWatchController stopFrame = new StopWatchController();
-					stopFrame.setVisible(true);
-					currFrame.dispose();
-				}
-			}
-		});
-		checkBoxStep.addItemListener(new ItemListener() {
-		
-			@Override
-			public void itemStateChanged(ItemEvent e) {
-				if (checkBoxStep.isSelected()) {
-					StepCounterController stepFrame = new StepCounterController();
-					stepFrame.setVisible(true);
-					currFrame.dispose();
-				}
-			}
-		});
-		checkBoxHRM.addItemListener(new ItemListener() {
-	    	
-			@Override
-			public void itemStateChanged(ItemEvent e) {
-				if (checkBoxHRM.isSelected()) {
-					//HRMController hRMFrame = new HRMController();
-					//hRMFrame.setVisible(true);
-					//currFrame.dispose();
-				}
-			}
-		});
-		checkBoxUser.addItemListener(new ItemListener() {
-	    	
-			@Override
-			public void itemStateChanged(ItemEvent e) {
-				if (checkBoxUser.isSelected()) {
-					UserController userFrame = new UserController();
-					userFrame.setVisible(true);
-					currFrame.dispose();
-				}
-			}
-		});
-	}
+    public ViewSelector() {
+    }
+  
 
     public void paintComponent(Graphics g) {
 
         // create new pane to paint from base Graphic obj g
         super.paintComponent(g);
         currPane = (Graphics2D) g;
-
+        
         currPane.setFont(new Font("Helvetica", Font.BOLD, 10));
         String stepText = String.format("Step Counter");
         String stopText = String.format("Stopwatch");
