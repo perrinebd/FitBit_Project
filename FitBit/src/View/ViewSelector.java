abd9edf9194a6ae4be91cf484995d4bf3f08156b
bradleylual <bradleyla@g.cofc.edu>
2017-11-29 22:05:57 -0500

    Initialized ViewSelector UI and its controller

--- FitBit/src/View/ViewSelector.java
+++ FitBit/src/View/ViewSelector.java
@@ -1,12 +1,35 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class ViewSelector {
-    JCheckBox checkBox1 = new JCheckBox();
-    JCheckBox checkBox2 = new JCheckBox();
-    JCheckBox checkBox3 = new JCheckBox();
-    JCheckBox checkBox4 = new JCheckBox();
+public class ViewSelector extends JPanel {
+    private Graphics2D currPane;
 
+    // pass the view a model to render
+    public ViewSelector() {
+    }
 
+    public void paintComponent(Graphics g) {
+
+        // create new pane to paint from base Graphic obj g
+        super.paintComponent(g);
+        currPane = (Graphics2D) g;
+
+        currPane.setFont(new Font("Helvetica", Font.BOLD, 40));
+        // render info
+        String stepText = String.format("Step Counter");
+        String stopText = String.format("Stopwatch");
+        String hrmText = String.format("Heart Rate Monitor");
+        String userText = String.format("User Login");
+        
+        currPane.drawString(stepText, 50, 50);
+        currPane.drawString(stopText, 100, 50);
+        currPane.drawString(hrmText, 50, 100);
+        currPane.drawString(userText, 100, 100);
+    }
+
+    public void removeComponent(){
+        currPane.dispose();
+    }
 }
