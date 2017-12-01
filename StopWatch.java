af105e684cee10cf0e0548e20ec48595f08a55a5
bradleylual <bradleyla@g.cofc.edu>
2017-12-01 14:36:53 -0500

    Revamped StepCounter

---
+++ StopWatch.java
@@ -0,0 +1,40 @@
+package View;
+
+import javax.swing.*;
+
+import Models.TimerModel;
+
+import java.awt.*;
+
+public class StopWatch extends JPanel {
+    private Models.TimerModel model;
+    private boolean display = true;
+    Graphics2D currPane;
+
+    // pass the view a model to render
+    public StopWatch(Models.TimerModel timer) {
+        this.model = timer;
+    }
+
+    public void paintComponent(Graphics g) {
+        // use override
+        super.paintComponent(g);
+        if(display) {
+            currPane = (Graphics2D) g;
+
+            // model info to display
+            int minutes = (int) model.getMinutes();
+            int seconds = (int) model.getSeconds();
+
+            currPane.setFont(new Font("Helvetica", Font.BOLD, 40));
+            // render info
+            String displayText = String.format("%d m : %d s", minutes, seconds);
+            currPane.drawString(displayText, 50, 50);
+        }
+    }
+
+    public void removeComponent(){
+        display = false;
+    }
+}
+
---
+++ ViewSelector.java
@@ -0,0 +1,51 @@
+package View;
+
+import javax.swing.*;
+
+import Controllers.StepCounterController;
+import Controllers.StopWatchController;
+import Controllers.UserController;
+
+import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class ViewSelector extends JPanel {
+    private Graphics2D currPane;
+    private JFrame currFrame;
+	private JPanel checkBoxPanel;
+	private JCheckBox checkBoxWatch;
+	private JCheckBox checkBoxStep;
+	private JCheckBox checkBoxHRM;
+	private JCheckBox checkBoxUser;
+	private Models.TimerModel clock;
+
+    public ViewSelector(Models.TimerModel timer) {
+    	this.clock = timer;
+    }
+  
+
+    public void paintComponent(Graphics g) {
+
+        // create new pane to paint from base Graphic obj g
+        super.paintComponent(g);
+        currPane = (Graphics2D) g;
+        
+        int clockHour = (int) clock.getHour();
+        
+        currPane.setFont(new Font("Helvetica", Font.BOLD, 10));
+        String stepText = String.format("Step Counter");
+        String stopText = String.format("Stopwatch");
+        String hrmText = String.format("Heart Rate Monitor");
+        String userText = String.format("User Login");
+        
+        currPane.drawString(stepText, 50, 50);
+        currPane.drawString(stopText, 100, 50);
+        currPane.drawString(hrmText, 50, 100);
+        currPane.drawString(userText, 100, 100);
+    }
+
+    public void removeComponent(){
+        currPane.dispose();
+    }
+}
